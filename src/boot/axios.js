ce10b5761b8d3c8a03455b5a620345b7f79d714a
Vladut Teodor <vladutteodor18@gmail.com>
2024-07-15 13:43:06 +0300

    api https

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -7,7 +7,7 @@ import {LocalStorage} from "quasar";
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'http://api.grocermate.ro', headers: {
+const api = axios.create({ baseURL: 'https://api.grocermate.ro', headers: {
     'Authorization': `Bearer ${LocalStorage.getItem('token')}`,
     'Accept': 'application/json',
   } })
